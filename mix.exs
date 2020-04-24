3015a5e41c3e96572bfa13f8b02219adb67f5ad6
Travis Roberts <traviskroberts@gmail.com>
2020-04-23 20:00:54 -0500

    Add logo to header

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Threddr.MixProject do
   def project do
     [
       app: :threddr,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
