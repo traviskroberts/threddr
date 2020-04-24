5348d3f2bc1f7ccb4bfc672aed86a226a414998e
Travis Roberts <traviskroberts@gmail.com>
2020-04-23 21:31:24 -0500

    Bump version for deploy

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Threddr.MixProject do
   def project do
     [
       app: :threddr,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
